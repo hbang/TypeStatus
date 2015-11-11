960fd2ebe124722e55bafe59c721e030882cca31
Adam Demasi <adam@hbang.com.au>
2015-11-12 00:53:22 +1030

    [prefs] move translation credits to to hashbang translations page

--- prefs/HBTSCreditsListController.m
+++ prefs/HBTSCreditsListController.m
@@ -8,4 +8,10 @@
 	return @"Credits";
 }
 
+#pragma mark - Callbacks
+
+- (void)openTranslations {
+	[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://www.hbang.ws/translations/"]];
+}
+
 @end
