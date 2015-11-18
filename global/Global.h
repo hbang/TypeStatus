f5f73e25afdb74f488f39683736e74e29e60133f
Ben Rosen <benrosen78@gmail.com>
2015-11-17 21:41:50 -0600

    well that didn't work

---
+++ global/Global.h
@@ -0,0 +1,37 @@
+typedef NS_ENUM(NSUInteger, HBTSStatusBarType) {
+	HBTSStatusBarTypeTyping,
+	HBTSStatusBarTypeTypingEnded,
+	HBTSStatusBarTypeRead
+};
+
+typedef NS_ENUM(NSUInteger, HBTSNotificationType) {
+	HBTSNotificationTypeNone,
+	HBTSNotificationTypeOverlay,
+	HBTSNotificationTypeIcon
+};
+
+typedef NS_ENUM(NSUInteger, HBTSStatusBarAnimation) {
+	HBTSStatusBarAnimationSlide,
+	HBTSStatusBarAnimationFade
+};
+
+static NSTimeInterval const kHBTSTypingTimeout = 60.0;
+
+
+// old values may be used here for compatibility with other tweaks that listen
+// for typestatus notifications
+
+static NSString *const HBTSClientSetStatusBarNotification = @"HBTSClientSetStatusBar";
+static NSString *const HBTSSpringBoardReceivedMessageNotification = @"HBTSSpringBoardReceivedMessageNotification";
+
+static NSString *const kHBTSMessageTypeKey = @"Type";
+static NSString *const kHBTSMessageSenderKey = @"Name";
+static NSString *const kHBTSMessageIsTypingKey = @"IsTyping";
+
+static NSString *const kHBTSMessageIconNameKey = @"IconName";
+static NSString *const kHBTSMessageTitleKey = @"Title";
+static NSString *const kHBTSMessageContentKey = @"Content";
+static NSString *const kHBTSMessageDirectionKey = @"Direction";
+
+static NSString *const kHBTSMessageTimeoutKey = @"Duration";
+static NSString *const kHBTSMessageSendDateKey = @"Date";
