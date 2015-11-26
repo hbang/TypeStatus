9c54ae916dd323d922fc9f886cc2a9c2263660c5
Adam Demasi <adam@hbang.com.au>
2015-11-26 14:24:06 +1030

    [global] move HBTSConversationPreferences to global/

--- global/HBTSConversationPreferences.h
+++ global/HBTSConversationPreferences.h
@@ -5,6 +5,9 @@
 - (BOOL)typingNotificationsEnabledForConversation:(CKConversation *)conversation;
 - (BOOL)readReceiptsEnabledForConversation:(CKConversation *)conversation;
 
+- (BOOL)typingNotificationsEnabledForHandle:(NSString *)handle;
+- (BOOL)readReceiptsEnabledForHandle:(NSString *)handle;
+
 - (void)setTypingNotificationsEnabled:(BOOL)enabled forConversation:(CKConversation *)conversation;
 - (void)setReadReceiptsEnabled:(BOOL)enabled forConversation:(CKConversation *)conversation;
 
--- global/HBTSConversationPreferences.m
+++ global/HBTSConversationPreferences.m
@@ -16,6 +16,15 @@
 	return self;
 }
 
+#pragma mark - External
+
+- (BOOL)_readReceiptsEnabled {
+	// the prefs bundle falls back to NO, so probably we should follow suit
+	return CFPreferencesGetAppBooleanValue(CFSTR("ReadReceiptsEnabled"), CFSTR("com.apple.madrid"), nil);
+}
+
+#pragma mark - Keys
+
 - (NSString *)_keyForConversation:(CKConversation *)conversation type:(NSString *)type {
 	if (!conversation._chatSupportsTypingIndicators || conversation.isGroupConversation) {
 		return nil;
@@ -24,30 +33,40 @@
 	return [NSString stringWithFormat:@"%@-%@", conversation.uniqueIdentifier, type];
 }
 
+- (NSString *)_keyForHandle:(NSString *)handle type:(NSString *)type {
+	return [NSString stringWithFormat:@"%@-%@", handle, type];
+}
+
 #pragma mark - Getters
 
 - (BOOL)typingNotificationsEnabledForConversation:(CKConversation *)conversation {
 	NSString *key = [self _keyForConversation:conversation type:@"Typing"];
-	return key ? [_preferences boolForKey:key default:YES] : YES;
+	return [_preferences boolForKey:key default:YES];
 }
 
 - (BOOL)readReceiptsEnabledForConversation:(CKConversation *)conversation {
-	BOOL globallyEnabled = YES;
-
 	NSString *key = [self _keyForConversation:conversation type:@"Read"];
-	return key ? [_preferences boolForKey:key default:globallyEnabled] : globallyEnabled;
+	return [_preferences boolForKey:key default:self._readReceiptsEnabled];
+}
+
+- (BOOL)typingNotificationsEnabledForHandle:(NSString *)handle {
+	NSString *key = [self _keyForHandle:handle type:@"Typing"];
+	return [_preferences boolForKey:key default:YES];
+}
+
+- (BOOL)readReceiptsEnabledForHandle:(NSString *)handle {
+	NSString *key = [self _keyForHandle:handle type:@"Read"];
+	return [_preferences boolForKey:key default:self._readReceiptsEnabled];
 }
 
 #pragma mark - Setters
 
-// TODO: needs implementing
-
 - (void)setTypingNotificationsEnabled:(BOOL)enabled forConversation:(CKConversation *)conversation {
-
+	[_preferences setBool:enabled forKey:[self _keyForConversation:conversation type:@"Typing"]];
 }
 
 - (void)setReadReceiptsEnabled:(BOOL)enabled forConversation:(CKConversation *)conversation {
-
+	[_preferences setBool:enabled forKey:[self _keyForConversation:conversation type:@"Read"]];
 }
 
 
