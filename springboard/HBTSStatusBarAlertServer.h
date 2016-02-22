b292c616cc0144774c40bcc00c8b7b11296afe88
Adam Demasi <adam@hbang.com.au>
2016-02-23 09:45:08 +1030

    [springboard] whoops, forgot to add this to the header

--- springboard/HBTSStatusBarAlertServer.h
+++ springboard/HBTSStatusBarAlertServer.h
@@ -1,5 +1,7 @@
 @interface HBTSStatusBarAlertServer : NSObject
 
++ (void)sendAlertWithIconName:(NSString *)iconName text:(NSString *)text boldRange:(NSRange)boldRange animatingInDirection:(BOOL)direction timeout:(NSTimeInterval)timeout;
+
 + (void)sendAlertWithIconName:(NSString *)iconName title:(NSString *)title content:(NSString *)content;
 + (void)sendAlertWithIconName:(NSString *)iconName title:(NSString *)title content:(NSString *)content animatingInDirection:(BOOL)direction timeout:(NSTimeInterval)timeout;
 
