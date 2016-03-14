f5a0df0646ac1c88da047e026aaaa5d023a314d9
Adam Demasi <adam@hbang.com.au>
2016-03-14 17:45:28 +1030

    [prefs] implement some of the table view stuff

--- prefs/HBTSMessagesListController.m
+++ prefs/HBTSMessagesListController.m
@@ -2,7 +2,10 @@
 #import "../global/HBTSConversationPreferences.h"
 #import <Preferences/PSSpecifier.h>
 
-@implementation HBTSMessagesListController
+@implementation HBTSMessagesListController {
+	UIBarButtonItem *_editBarButtonItem;
+	UIBarButtonItem *_doneBarButtonItem;
+}
 
 + (NSString *)hb_specifierPlist {
 	return @"Messages";
@@ -12,12 +15,19 @@
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
+
+	_editBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemEdit target:self action:@selector(_editTapped:)];
+	_doneBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(_editTapped:)];
+
+	self.navigationItem.rightBarButtonItem = _editBarButtonItem;
+
 	[self _disableIfNeeded];
 	[self _insertPeople];
 }
 
 - (void)reloadSpecifiers {
 	[super reloadSpecifiers];
+
 	[self _disableIfNeeded];
 	[self _insertPeople];
 }
@@ -45,6 +55,11 @@
 
 #pragma mark - Callbacks
 
+- (void)_editTapped:(UIBarButtonItem *)sender {
+	[self.table setEditing:!self.table.editing animated:YES];
+	self.navigationItem.rightBarButtonItem = self.table.editing ? _doneBarButtonItem : _editBarButtonItem;
+}
+
 - (void)addPerson {
 	// TODO: can this be autoreleased?
 	CNContactPickerViewController *pickerController = [[[CNContactPickerViewController alloc] init] autorelease];
@@ -66,6 +81,29 @@
 	for (CNLabeledValue<NSString *> *value in contact.emailAddresses) {
 		[values addObject:value.value];
 	}
+}
+
+#pragma mark - Table View
+
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+	return indexPath.section == 1;
+}
+
+- (UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath {
+	return UITableViewCellEditingStyleDelete;
+}
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+	HBLogDebug(@"commitEditingStyle %@", indexPath);
+}
+
+#pragma mark - Memory management
+
+- (void)dealloc {
+	[_editBarButtonItem release];
+	[_doneBarButtonItem release];
+
+	[super dealloc];
 }
 
 @end
